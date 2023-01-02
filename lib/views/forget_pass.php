8618ca76ff3d3d87ca24f43a7ffc403969de6dd6
JehanKandy <jehankandy@gmail.com>
2023-01-02 20:38:00 +0530

    Add files via upload

---
+++ lib/views/forget_pass.php
@@ -0,0 +1,17 @@
+<?php include("../layouts/header.php"); ?>
+<link rel="stylesheet" href="../../css/">
+
+<div class="container">
+    <div class="login-content">
+        <div class="title">
+            <i class="fas fa-user-plus"></i> Register Here
+        </div>
+        <div class="body">
+        hi
+        </div>
+    </div>
+</div>
+
+
+<script src="../../js/script.js"></script>
+<?php include("../layouts/footer.php"); ?>
