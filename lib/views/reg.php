03e4a30b5796245a7a3b84c20e5330cecd362471
JehanKandy <jehankandy@gmail.com>
2023-01-02 20:02:59 +0530

    Update reg.php

--- lib/views/reg.php
+++ lib/views/reg.php
@@ -7,18 +7,23 @@
             <i class="fas fa-user-plus"></i> Register Here
         </div>
         <div class="body">
-            <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST" name="reg_form" onsubmit="return valodateReg();">
+            <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST" name="reg_form" onsubmit="return valodateReg(); ">
                 <p class="form-text" id="regUser">Username :</p>
                 <input type="text" name="uername" id="Userreg" class="form-input">
+                <p id="reguserError"></p>
 
                 <p class="form-text" id="regEmail">Email :</p>
                 <input type="email" name="email" id="Useremail" class="form-input">
+                <p id="regemailError"></p>
 
                 <p class="form-text" id="regpass">Password : </p>
                 <input type="password" name="pass" id="Userpass" class="form-input">
+                <p id="regpassError"></p>
+                <p class="checkpass"><span id="p1" class="part1"></span><span id="p2" class="part2"></span><span id="p3" class="part3"></span></p>
 
                 <p class="form-text" id="regcpass">Confirm Password</p>
                 <input type="password" id="Usercpass" class="form-input">
+                <p id="regcpassError"></p>
 
                 <p style="margin-top: 15px;"><input type="reset" value="Clear" class="btn btn-secondary reg-btns">&nbsp;&nbsp;<input type="submit" value="Sign Up" class="btn btn-success reg-btns"></p>
             </form>
