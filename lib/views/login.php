e56230c02d02cd2ec69bcf492be15c4550db3e1e
JehanKandy <jehankandy@gmail.com>
2023-01-02 17:07:55 +0530

    Add files via upload

---
+++ lib/views/login.php
@@ -0,0 +1,14 @@
+<?php include("../layouts/header.php"); ?>
+<link rel="stylesheet" href="../../css/style.css">
+
+<div class="container">
+    <div class="login-content">
+        <div class="title">
+            <i class="fas fa-user-alt"></i> Login Here
+        </div>
+    </div>
+</div>
+
+
+<?php include("../layouts/footer.php"); ?>
+<script src="../../js/script.js"></script>
