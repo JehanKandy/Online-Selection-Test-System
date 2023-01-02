f9a68ddabd2e05007b420cd16b0fb0592f521909
JehanKandy <jehankandy@gmail.com>
2023-01-02 21:08:40 +0530

    Add files via upload

---
+++ lib/views/waiting_user.php
@@ -0,0 +1,17 @@
+<?php include("../layouts/header.php"); ?>
+<link rel="stylesheet" href="../../css/style.css">
+
+<div class="container">
+    <div class="login-content">
+        <div class="title">
+            <i class="fas fa-key"></i> Verify OTP
+        </div>
+        <div class="body">
+
+        </form>            
+        </div>
+    </div>
+</div>
+
+<script src="../../js/script.js"></script>
+<?php include("../layouts/footer.php"); ?>
