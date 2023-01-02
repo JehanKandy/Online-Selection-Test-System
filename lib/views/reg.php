e8c5ed17362da0558a427cb09c68799bbe5c6c8d
JehanKandy <jehankandy@gmail.com>
2023-01-02 18:01:40 +0530

    Update reg.php

--- lib/views/reg.php
+++ lib/views/reg.php
@@ -7,11 +7,14 @@
             <i class="fas fa-user-alt"></i> Login Here
         </div>
         <div class="body">
-            
+            <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST" name="reg_form" onsubmit="return valodateReg();">
+                <p class="form-text" id="regUser">Username :</p>
+        
+            </form>
         </div>
     </div>
 </div>
 
 
 <script src="../../js/script.js"></script>
-<?php include("../layouts/header.php"); ?>
+<?php include("../layouts/footer.php"); ?>
