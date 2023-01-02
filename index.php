06d84563bfccd534f8fc69930f802d46e70714cc
JehanKandy <jehankandy@gmail.com>
2023-01-02 16:58:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php include("lib/layouts/header.php"); ?>
 <link rel="stylesheet" href="css/style.css">
 
+
+<div class="home-content">
+    <div class="container">
+        <div class="title">
+            Online Selection Examination System
+        </div>
+        <div class="body">
+            <p class="text"> For Attempt the Examination, click here  </p>
+            <a href=""><button class="btn btn-primary">Attempt Examination</button></a>
+            <br>
+        </div>
+    </div>
+</div>
+
 <?php include("lib/layouts/footer.php"); ?>
 <script src="js/script.js"></script>
