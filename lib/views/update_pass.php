2a317d110a1c94fcf4f78c3d9a304f6678bf0d82
JehanKandy <jehankandy@gmail.com>
2023-01-02 21:06:07 +0530

    Update update_pass.php

--- lib/views/update_pass.php
+++ lib/views/update_pass.php
@@ -7,7 +7,21 @@
             <i class="fas fa-key"></i> Verify OTP
         </div>
         <div class="body">
+            <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
+                <p class="form-text">Username : </p>
+                <input type="text" name="pass_username" id="" class="form-input">
 
+                <p class="form-text">Email : </p>
+                <input type="email" name="pass_email" id="" class="form-input">
+        
+                <p class="form-text">Password : </p>
+                <input type="password" name="pass_pass" id="" class="form-input">
+
+                <p class="form-text">Confirm Password : </p>
+                <input type="password" name="pass_cpass" id="" class="form-input">
+
+                <input type="submit" value="Update Password" name="update_pass" class="btn btn-success btn-lg btn-block" style="margin-top: 10px;">
+            </form>
         </div>
     </div>
 </div>
