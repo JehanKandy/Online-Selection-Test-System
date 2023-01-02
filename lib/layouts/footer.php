93b4a4b9b65e75511b518f70bbd65ce8139dc54a
JehanKandy <jehankandy@gmail.com>
2023-01-02 06:50:04 +0530

    Update footer.php

--- lib/layouts/footer.php
+++ lib/layouts/footer.php
@@ -1,6 +1,7 @@
 <?php include("header.php"); ?>
 <link rel="stylesheet" href="../../css/style.css">
 
+<hr class="footer-hr">
 <div class="footer-content">
     Copyright <i class="far fa-copyright"></i> 2021 – 2023 <a href="https://github.com/JehanKandy" target="_blank" style="text-decoration: none;">JehanKandy</a>, <a href="https://github.com/JehanKandy/Online-Selection-Test-System" target="_blank" style="text-decoration: none;">Online Selection Examination System</a>
 </div>
