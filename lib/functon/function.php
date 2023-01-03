f2e99f10d6b9836031b7ed6b11676f40b2d95d94
JehanKandy <jehankandy@gmail.com>
2023-01-04 00:52:07 +0530

    Update function.php

--- lib/functon/function.php
+++ lib/functon/function.php
@@ -17,7 +17,11 @@
         if($check_user_nor == 0){
             $insert_user = "INSERT INTO user_tbl (username,email,user_pass,user_type,is_pending,is_active,join_date)VALUES('$username','$email','$pass','user',1,0,NOW())";
             $insert_user_result = mysqli_query($con, $insert_user);
-            
+
+            if(!$insert_user_result){
+
+            }
+
         }else{
             return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
                     <strong>Username Error</strong>User Already Exists...!
