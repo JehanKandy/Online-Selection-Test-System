0049652ac1b48fc4b3072d90ba1ff3d0d86c7ef2
JehanKandy <jehankandy@gmail.com>
2023-01-04 00:41:12 +0530

    Update function.php

--- lib/functon/function.php
+++ lib/functon/function.php
@@ -7,8 +7,12 @@
     session_start();
 
 
-    function reg_user(){
-        $con = Connection
+    function reg_user($username,$email,$pass){
+        $con = Connection();
+
+        $check_user = "SELECT * FROM user_tbl WHERE username  name = '$username' && email = '$email'";
+        $check_user_result = mysqli_query($con, $check_user);
+        
     }
 
 ?>
