d3e731949a376022bf7cefef23889f494391e86d
JehanKandy <jehankandy@gmail.com>
2023-01-04 07:22:55 +0530

    Update function.php

--- lib/functon/function.php
+++ lib/functon/function.php
@@ -1,34 +1,137 @@
-<?php include("../layouts/header.php"); ?>
-<link rel="stylesheet" href="../../css/">
+<?php
 
-<div class="container">
-    <div class="login-content">
-        <div class="title">
-            <i class="fas fa-key"></i> Get OTP
-        </div>
-        <div class="body">
-            <?php 
-                include("../functon/function.php");
+    use FTP\Connection;
 
-                if(isset($_POST['get_otp'])){
-                    $result = request_otp($_POST['otp_username'], $_POST['otp_email']);
-                    echo $result;
-                }           
-            ?>
+    include("config.php");
 
-            <form action="<?php echo($_SERVER['PHP_SLEF']); ?>" method="POST">
-                <p class="form-text">Username : </p>
-                <input type="text" name="otp_username" id="" class="form-input">
-
-                <p class="form-text">Password : </p>
-                <input type="email" name="otp_email" id="" class="form-input">
-
-                <input type="submit" value="Request OTP" class="btn btn-primary" name="get_otp" style="margin-top: 10px;">
-            </form>
-        </div>
-    </div>
-</div>
+    session_start();
 
 
-<script src="../../js/script.js"></script>
-<?php include("../layouts/footer.php"); ?>
+    function reg_user($username,$email,$pass){
+        $con = Connection();
+
+        $check_user = "SELECT * FROM user_tbl WHERE username  = '$username' && email = '$email'";
+        $check_user_result = mysqli_query($con, $check_user);
+        $check_user_nor = mysqli_num_rows($check_user_result);
+
+        if($check_user_nor == 0){
+            $insert_user = "INSERT INTO user_tbl (username,email,user_pass,user_type,is_pending,is_active,join_date)VALUES('$username','$email','$pass','student',1,0,NOW())";
+            $insert_user_result = mysqli_query($con, $insert_user);
+
+            if($insert_user_result){
+                return  "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+                        <strong>Successfully</strong>User Create Successfully <a href='login.php' style='text-decoration:none;'>Login</a>
+                        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                        <span aria-hidden='true'>&times;</span>
+                        </button>
+                </div>";
+            }else{
+                return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                        <strong>Sign Up Error</strong>Can not Process the Task...!
+                        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                        <span aria-hidden='true'>&times;</span>
+                        </button>
+                </div>";
+            }
+
+        }else{
+            return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                    <strong>Username Error</strong>User Already Exists...!
+                    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>&times;</span>
+                    </button>
+            </div>";
+        }
+
+    }
+
+    function login_user($username, $pass){
+        $con = Connection();
+
+        $check_user_pending = "SELECT * FROM user_tbl WHERE username = '$username' && user_pass = '$pass' && is_pending = 1 && is_active = 0";
+        $check_user_pending_result = mysqli_query($con, $check_user_pending);
+        $check_user_pending_nor = mysqli_num_rows($check_user_pending_result);
+
+        $_SESSION['userName'] = $username; 
+
+        if($check_user_pending_nor > 0){
+            header("location:waiting_user.php");
+        }else{
+            $check_user_deactive = "SELECT * FROM user_tbl WHERE username = '$username' && user_pass = '$pass' && is_pending = 0 && is_active = 0";
+            $check_user_deactive_result = mysqli_query($con, $check_user_deactive);
+            $check_user_deactive_nor = mysqli_num_rows($check_user_deactive_result);
+
+            if($check_user_deactive_nor > 0){
+                return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                    <strong>Account Error</strong>Account Deactive...!
+                    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>&times;</span>
+                    </button>
+            </div>";
+            }
+            else{
+                $login_user = "SELECT * FROM user_tbl WHERE username = '$username' && user_pass = '$pass' && is_pending = 0 && is_active = 1";
+                $login_user_result = mysqli_query($con, $login_user);
+                $login_user_nor = mysqli_num_rows($login_user_result);
+                $login_user_row = mysqli_fetch_assoc($login_user_result);
+
+                if($login_user_nor > 0){
+                    if($login_user_row['user_type'] == "student"){
+                        setcookie('login',$login_user_row['email'],time()+60*60,'/');
+                        $_SESSION['LoginSession'] = $login_user_row['email'];
+                        header("location:../routes/student.php");
+                    }elseif($login_user_row['user_type'] == "teacher"){
+                        setcookie('login',$login_user_row['email'],time()+60*60,'/');
+                        $_SESSION['LoginSession'] = $login_user_row['email'];
+                        header("location:../routes/teacher.php");
+                    }
+                    elseif($login_user_row['user_type'] == "admin"){
+                        setcookie('login',$login_user_row['email'],time()+60*60,'/');
+                        $_SESSION['LoginSession'] = $login_user_row['email'];
+                        header("location:../routes/admin.php");
+                    }
+                }else{
+                    return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                                <strong>User Error</strong>User Does not Exist..!
+                                <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                                <span aria-hidden='true'>&times;</span>
+                                </button>
+                        </div>";
+                }
+            }
+        }
+    } 
+    function view_username(){
+        $con = Connection();
+
+        $useremail = strval($_SESSION['userName']);
+        echo $useremail;
+    }
+
+    function request_otp($username, $email){
+        $con = Connection();
+
+        $check_user_otp = "SELECT * FROM user_tbl WHERE username='$username' && email='$email' && is_pending = 0 && is_active = 0";
+        $check_user_otp_result = mysqli_query($con, $check_user_otp);
+        $check_user_otp_nor = mysqli_num_rows($check_user_otp_result);
+        
+
+        if(empty($username)){
+            return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                    <strong>Username Error</strong>Username Does not Exist..!
+                    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>&times;</span>
+                    </button>
+            </div>";
+        }
+        if(empty($email)){
+            return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                    <strong>Email Error</strong>Email Does not Exist..!
+                    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>&times;</span>
+                    </button>
+            </div>";
+        }
+    }
+
+?>
