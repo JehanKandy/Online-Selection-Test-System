707856684b170f6b79f616abecb3929e56b1c155
JehanKandy <jehankandy@gmail.com>
2023-01-04 14:04:54 +0530

    Update function.php

--- lib/functon/function.php
+++ lib/functon/function.php
@@ -267,7 +267,16 @@
             </div>";
         }
         else{
-            
+            $check_user = "SELECT * FROM user_tbl WHERE username='$username' && user_pass='$email'";
+            $check_user_result = mysqli_query($con, $check_user);
+            $check_user_row = mysqli_num_rows($check_user_result);
+            $check_user_nor = mysqli_fetch_assoc($check_user_result);
+
+            if($check_user_nor > 0){
+
+            }else{
+
+            }
         }
     }
 
