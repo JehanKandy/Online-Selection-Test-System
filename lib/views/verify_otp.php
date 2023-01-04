5f31ac01fc57b2abc461602f3dbec2c1fb1a0bf2
JehanKandy <jehankandy@gmail.com>
2023-01-04 08:03:49 +0530

    Update verify_otp.php

--- lib/views/verify_otp.php
+++ lib/views/verify_otp.php
@@ -7,6 +7,16 @@
             <i class="fas fa-key"></i> Verify OTP
         </div>
         <div class="body">
+            <?php 
+                include("../functon/function.php");
+
+                if(isset($_POST['verify_otp'])){
+                    $result = verify_otp(md5($_POST['check_otp']));
+                    echo $result;
+                }
+            
+            ?>
+
             <form action="<?php echo($_SERVER['PHP_SELF']); ?>" method="POST">
                 <p class="form-text">Enter OTP : </p>
                 <input type="number" name="check_otp" id="" class="form-input">
