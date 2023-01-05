5fbc81283ce75ccb0d4f7761dbe5c8af57ae8664
JehanKandy <jehankandy@gmail.com>
2023-01-05 21:38:42 +0530

    Update exam.php

--- lib/routes/exam.php
+++ lib/routes/exam.php
@@ -2,13 +2,20 @@
 <?php include("../functon/function.php"); ?>
 <link rel="stylesheet" href="../../css/style.css">
 
+<?php 
+    $_SESSION['examStart'] = time();
+
+
+?>
+
+
 <div class="exam-content">
     <div class="container">
         <div class="title">
             Online Selection Examination
         </div>
         <div class="body">
-            
+
         </div>
     </div>
 </div>
