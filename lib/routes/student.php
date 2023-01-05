eeed16af92cc79048c92965d582782cc123d7092
JehanKandy <jehankandy@gmail.com>
2023-01-05 21:22:12 +0530

    Update student.php

--- lib/routes/student.php
+++ lib/routes/student.php
@@ -90,7 +90,24 @@
                 <div class="title">Your Result</div>
 
                 <div class="body">
-
+                    <div class="table-responsive-lg">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Username</th>
+                                    <th>Marks</th>
+                                    <th>Attempt time</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Jehan</td>
+                                    <td>40</td>
+                                    <td>2023/01/05 23:10:52</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
 
