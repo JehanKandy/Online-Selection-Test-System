d32cbff3d545be378f80eae458078f945fba4fd3
JehanKandy <jehankandy@gmail.com>
2023-01-05 07:28:31 +0530

    Update student.php

--- lib/routes/student.php
+++ lib/routes/student.php
@@ -1,15 +1,24 @@
 <?php include("../layouts/header.php"); ?>
 <link rel="stylesheet" href="../../css/style.css">
+<?php include("../layouts/nav_loged.php"); ?>
 
 <?php 
     include("../functon/function.php");
-
-    if($_SESSION['LoginSession']){
-        header("location:../views/logout.php");
+    if(empty($_SESSION['LoginSession'])){
+        header("location:../views/login.php");
     }
 ?>
 
+<div class="std-content">
+    <div class="container">
+        <div class="title">Welcome Come to Online Selection Test</div>
+        <hr>
 
-
+        <div class="body">
+            <h3>Studnet details</h3>
+        
+        </div>
+    </div>
+</div>
 
 <?php include("../layouts/footer.php"); ?>
