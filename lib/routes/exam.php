7922539a0164790d3e9abc45035bb2f1465153ce
JehanKandy <jehankandy@gmail.com>
2023-01-05 21:32:08 +0530

    Update exam.php

---
+++ lib/routes/exam.php
@@ -0,0 +1,6 @@
+<?php include("../layouts/header.php"); ?>
+<?php include("../functon/function.php"); ?>
+<link rel="stylesheet" href="../../css/style.css">
+
+
+<?php include("../layouts/footer.php"); ?>
