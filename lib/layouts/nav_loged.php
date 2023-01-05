c9a86bf1c5be8ec902d1204f5e6316a6ea3a6fd2
JehanKandy <jehankandy@gmail.com>
2023-01-05 09:23:25 +0530

    Update nav_loged.php

--- lib/layouts/nav_loged.php
+++ lib/layouts/nav_loged.php
@@ -1,7 +1,10 @@
+<?php include("../functon/function.php"); ?>
 <div class="container">
     <nav>
         <div class="nav-content">
             Online Selection Test
+
+
 
             <a href="../views/logout.php"><button class="btn btn-danger">Logout</button></a>
         </div>
