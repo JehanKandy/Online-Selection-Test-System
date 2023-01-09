57a6c4fd0ee2bbb3ffbb7472c5fc550590500b61
JehanKandy <jehankandy@gmail.com>
2023-01-09 06:42:20 +0530

    Update view_user.php

--- lib/routes/view_user.php
+++ lib/routes/view_user.php
@@ -14,8 +14,32 @@
 <div class="view-user">
     <div class="container">
         <div class="title"> User : <?php view_user_email(); ?></div>
-        <div class="body">
+        <hr>
+        
+        <?php 
+            if(isset($_POST['to_deactive_form'])){
+                $result = to_deactive_user($_POST['to_deactive']);
+                echo $result;
+            }
+            elseif(isset($_POST['to_active_form'])){
+                $result = to_active_user($_POST['to_active']);
+                echo $result;
+            }
+            elseif(isset($_POST['to_pending_user'])){
+                $result = to_pending_user($_POST['to_pending']);
+                echo $result;
+            }
+            elseif(isset($_POST['to_approved_form'])){
+                $result = to_apprived_user($_POST['to_approved']);
+                echo $result;
+            }
 
+            
+            
+        ?>
+
+        <div class="body">
+            <?php user_data(); ?>
         </div>
     </div>
 </div>
