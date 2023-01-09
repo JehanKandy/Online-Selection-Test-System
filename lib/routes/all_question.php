b12af7ff1287fcfc1ae5d3bf68ada3883585a672
JehanKandy <jehankandy@gmail.com>
2023-01-09 09:23:07 +0530

    Update all_question.php

--- lib/routes/all_question.php
+++ lib/routes/all_question.php
@@ -1,6 +1,4 @@
-<?php include("../layouts/header.php"); ?>
 <link rel="stylesheet" href="../../css/style.css">
-<?php include("../layouts/nav_loged.php"); ?>
 
 <?php 
     if(empty($_SESSION['LoginSession'])){
@@ -8,19 +6,28 @@
     }
     
     admin_access();
+    techer_access();
 ?>
+
+<div class="all-question">
+    <div class="title">All Questions</div>
+    <hr>
+</div>
 
 <div class="table-responsive-lg">
     <table class="table">
         <thead>
             <tr>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Delete Date</th>
+                <th>Question</th>
+                <th>Add User</th>
+                <th>Status</th>
+                <th>Pending Status</th>
+                <th>Add Date</th>
+                <th></th>
             </tr>
         </thead>
         <tbody>
-            <?php all_deleted_approvels(); ?>
+            <?php view_all_questions(); ?>
         </tbody>
     </table>
 </div>
@@ -29,4 +36,3 @@
 
 
 <script src="../../js/script.js"></script>
-<?php include("../layouts/footer.php"); ?>
