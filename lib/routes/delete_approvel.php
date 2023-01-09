ca3c3243cacb18b39dc47ff0824609cef01efc0f
JehanKandy <jehankandy@gmail.com>
2023-01-09 07:15:18 +0530

    Add files via upload

---
+++ lib/routes/delete_approvel.php
@@ -0,0 +1,25 @@
+<?php include("../layouts/header.php"); ?>
+<link rel="stylesheet" href="../../css/style.css">
+<?php include("../layouts/nav_loged.php"); ?>
+
+<?php 
+    if(empty($_SESSION['LoginSession'])){
+        header("location:../views/login.php");
+    }
+    
+    admin_access();
+
+?>
+
+<div class="delete-approvel">
+    <div class="continer">
+        <div class="title">Delete Approval : </div>
+        <hr>
+        <div class="body">
+
+        </div>
+    </div>
+</div>
+
+<script src="../../js/script.js"></script>
+<?php include("../layouts/footer.php"); ?>
